a562bf29928defbef32725bea6cfa485079173b1
hala-s <hala_mallak6522@hotmail.com>
2024-08-21 19:09:53 +0300

    first commit

---
+++ SrudentMVC/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SrudentMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult getAll()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
